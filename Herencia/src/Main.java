7c0362f9fdf377885e849e2d8bbc4454ed788a19
Luis  Alberto Jaramillo Pulido <luisjaramillopulido@gmail.com>
2022-04-24 20:15:47 +0200

    ee
    eeee

--- Herencia/src/Main.java
+++ Herencia/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Alumno al = new Alumno();
-		al.setNombre("Bill Gates");
+		al.setNombre("Juanillo");
 		al.setDni(12312312);
 		al.setCreditosAprobados(9);
 		System.out.println(al);
